d164472a2b375bca94933b50f949a28b4e084091
Spencer Hamilton <speham@gmail.com>
2022-04-07 15:13:08 -0500

    first two completed

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,10 +1,23 @@
-'use strict';
+"use strict";
 
 //Complete this algo
 const isLoop = (linkedlist) => {
-
+  let slow = linkedlist.head;
+  if (slow === null) {
+    return false;
+  }
+  let fast = slow.next;
+  while (fast !== slow) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+    if (fast === null || fast.next === null) {
+      return false;
+    }
+  }
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +27,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,25 @@
-'use strict';
+"use strict";
 
 // Complete this algo
-const minJumps = arr => {
+const minJumps = (arr) => {
+  let start = 0;
+  let jumpCounter = 0;
+  let myArray = arr.slice(start + 1, arr[start]);
 
+  while (start <= arr.length - 1) {
+    let biggestJumpNum = Math.max(...myArray);
+
+    start += myArray.indexOf(biggestJumpNum);
+
+    jumpCounter++;
+  }
+  return jumpCounter;
 };
 
-module.exports = minJumps
+module.exports = minJumps;
+
+// 1. Start at [0]
+// 2. look at all numbers between Array.0 and Array.n
+// 3. Get biggest value of that slice
+// 4. make that the new starting point
+// 5. repeat until starting point is >= Array.length - 1
